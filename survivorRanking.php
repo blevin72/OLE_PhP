d4dbc5c5d92f088da882e6d72ef789a66788541e
blevin72 <135919677+blevin72@users.noreply.github.com>
2024-03-22 06:57:31 -0700

    Update survivorRanking.php

--- survivorRanking.php
+++ survivorRanking.php
@@ -9,16 +9,16 @@ if(mysqli_connect_errno()){
 
 $action = isset($_GET['action']) ? $_GET['action'] : '';
 
-if($action == 'get_character')
-if($action == 'get_character')
+if($action == 'get_characterRanking')
 {
     //get settings based off characterID
     $characterID = isset($_GET['characterID']) ? ($_GET['characterID']) : 0;
 
-    $getLevelAndExpQuery = "SELECT `level`, exp, characterID FROM characters
+    $query = "SELECT `level`, exp, outpost_ranking, total_health, total_stamina, total_protection, total_progression, 
+    strength, dexterity, intellect, endurance, charm, stealth FROM characters
     WHERE characterID = '$characterID'";
 
-    $result= mysqli_query($con, $getLevelAndExpQuery);
+    $result= mysqli_query($con, $query);
 
     if($result)
     {
@@ -26,8 +26,20 @@ if($action == 'get_character')
         $response = array(
             'characterID' => $row['characterID'],
             'level' => $row['level'],
-            'exp' => $row['exp']
-        );
+            'exp' => $row['exp'],
+            'outpost_ranking' => $row['outpost_ranking'],
+            'total_health'=> $row['total_health'],
+            'total_stamina'=> $row['total_stamina'],
+            'total_protection'=> $row['total_protection'],
+            'total_progression'=> $row['total_progression'],
+            'strength'=> $row['strength'],
+            'dexterity'=> $row['dexterity'],
+            'intellect'=> $row['intellect'],
+            'endurance'=> $row['endurance'],
+            'charm'=> $row['charm'],
+            'stealth'=> $row['stealth']
+        );   
+
         // Set response headers to indicate JSON content
         header('Content-Type: application/json');
          
