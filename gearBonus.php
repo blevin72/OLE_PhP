f49b965647fa5f10ac3d92ea5c7c99c84c183b82
blevin72 <135919677+blevin72@users.noreply.github.com>
2024-04-14 17:26:56 -0700

    Update gearBonus.php
    Added remaining queries

--- gearBonus.php
+++ gearBonus.php
@@ -18,7 +18,60 @@ if($action == 'meleeQuery')
     $meleeData = mysqli_fetch_assoc($result);
     echo json_encode(array('meleeBonus' => $meleeData['meleeModifier']));
 }
-
+//-----------------------------CRAFT-----------------------------//
+if($action == 'craftQuery')
+{
+    $craftQuery = "SELECT craftModifier from gear_hands WHERE gearName = (SELECT equipped_hands FROM characters WHERE characterID = '$characterID')";
+    $result = mysqli_query($con, $craftQuery);
+    $craftData = mysqli_fetch_assoc($result);
+    echo json_encode(array('craftBonus' => $craftData['craftModifier']));
+}
+//-----------------------------STAMINA-----------------------------//
+if($action == 'staminaQuery')
+{
+    $staminaQuery = "SELECT staminaModifier from gear_feet WHERE gearName = (SELECT equipped_feet FROM characters WHERE characterID = '$characterID')";
+    $result = mysqli_query($con, $staminaQuery);
+    $staminaData = mysqli_fetch_assoc($result);
+    echo json_encode(array('staminaBonus' => $staminaData['staminaModifier']));
+}
+//-----------------------------IMMUNITY-----------------------------//
+if($action == 'immunityQuery')
+{
+    $immunityQuery = "SELECT immunityModifier from gear_head WHERE gearName = (SELECT equipped_head FROM characters WHERE characterID = '$characterID')";
+    $result = mysqli_query($con, $immunityQuery);
+    $immunityData = mysqli_fetch_assoc($result);
+    echo json_encode(array('immunityBonus' => $immunityData['immunityModifier']));
+}
+//-----------------------------AGRO-----------------------------//
+if($action == 'agroQuery')
+{
+    $agroQuery = "SELECT agroModifier from gear_feet WHERE gearName = (SELECT equipped_feet FROM characters WHERE characterID = '$characterID')";
+    $result = mysqli_query($con, $agroQuery);
+    $agroData = mysqli_fetch_assoc($result);
+    echo json_encode(array('agroBonus' => $agroData['agroModifier']));
+}
+//-----------------------------SPEED-----------------------------//
+if($action == 'speedQuery')
+{
+    $speedQuery = "SELECT 
+                f.speedModifier AS feetSpeed,
+                l.speedModifier AS legsSpeed,
+                t.speedModifier as torsoSpeed
+                FROM
+                characters c
+                LEFT JOIN
+                gear_feet f ON c.equipped_feet = f.gearName
+                LEFT JOIN
+                gear_legs l ON c.equipped_legs = l.gearName
+                LEFT JOIN
+                gear_torso t ON c.equipped_torso = t.gearName
+                WHERE
+                c.characterID = '$characterID'";
+    $result = mysqli_query($con, $speedQuery);
+    $speedData = mysqli_fetch_assoc($result);
+    $speed = $speedData['feetSpeed'] + $speedData['legsSpeed'] + $speedData['torsoSpeed'];
+    echo json_encode(array('speedBonus' => $speed));
+}
 //-----------------------------PROTECTION-----------------------------//
 if($action == 'protectionQuery')
 {
